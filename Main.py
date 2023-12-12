fbb478a887b31fb2efb582f4f61d2e4b3d2ef4cc
SergioSKA27 <sergioska81@hotmail.com>
2023-12-12 01:14:24 -0600

    Add Python logo and club logos to the application

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import hydralit_components as hc
+import base64
 
 st.set_page_config(layout="wide")
 st.markdown("""
@@ -33,6 +34,48 @@ background-color: #e5e5f7;
 """,unsafe_allow_html=True)
 
 
+#---------------------------------#
+#Functions
+def pythonlogo():
+    """
+    The `pythonlogo` function displays the Python logo using SVG code and a CSS file.
+    """
+    with open('rsc/css/style_PythonLogo.css') as f:
+        container1 = st.empty()
+        columns1,col2,col3 = st.columns(3)
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        with columns1:
+            container1.markdown(
+    '''<svg width="25%" height="25%" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" id="python-logo">
+    <path d="m116 296c0-30.328125 24.671875-55 55-55h170c13.785156 0 25-11.214844 25-25v-141c0-41.355469-33.644531-75-75-75h-70c-41.355469 0-75 33.644531-75 75v41h110c8.285156 0 15 6.714844 15 15s-6.714844 15-15 15h-181c-41.355469 0-75 33.644531-75 75v70c0 41.355469 33.644531 75 75 75h41zm105-220c-8.285156 0-15-6.714844-15-15s6.714844-15 15-15 15 6.714844 15 15-6.714844 15-15 15zm0 0" />
+    <path d="m437 146h-41v70c0 30.328125-24.671875 55-55 55h-170c-13.785156 0-25 11.214844-25 25v141c0 41.355469 33.644531 75 75 75h70c41.355469 0 75-33.644531 75-75v-41h-110c-8.285156 0-15-6.714844-15-15s6.714844-15 15-15h181c41.355469 0 75-33.644531 75-75v-70c0-41.355469-33.644531-75-75-75zm-146 290c8.285156 0 15 6.714844 15 15s-6.714844 15-15 15-15-6.714844-15-15 6.714844-15 15-15zm0 0" />
+    </svg>''',unsafe_allow_html=True
+            )
+
+
+def show_logos():
+    """
+    The `show_logos` function displays the Python logo and the name of a Python club using SVG code and a CSS file.
+    """
+
+
+    columns1,col2,col3 = st.columns(3)
+
+    with col2:
+        st.write('## Club de Algoritmia en Python Avanzado' )
+    with columns1:
+        file_ = open("rsc/Logos/FESA_LOGO.png",'rb')
+        contents = file_.read()
+        data_url = base64.b64encode(contents).decode("utf-8")
+        file_.close()
+        st.markdown(
+                f'<div style="text-align: center;"><img src="data:image/gif;base64,{data_url}" alt="logo-fes-mac"></div>',
+                unsafe_allow_html=True,
+                )
+    with col3:
+        pythonlogo()
+
+
 
 
 menu_data = [
@@ -60,3 +103,8 @@ menu_id = hc.nav_bar(
         sticky_nav=False, #at the top or not
         sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
     )
+
+
+
+
+show_logos()
