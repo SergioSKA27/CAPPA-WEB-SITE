65092d82bff5e3b4ec6ecfc27822d941c071bf95
SergioSKA27 <sergioska81@hotmail.com>
2023-12-19 00:31:50 -0600

    Add login page with background animation

---
+++ pages/login.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import bcrypt as bc
+
+st.set_page_config(page_title='Login', page_icon=':lock:', layout='centered')
+
+with open('rsc/css/backgroundLogin.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    st.markdown('''
+<div class="stars">
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+</div>''',unsafe_allow_html=True)
+
+
+with st.form(key='login_form'):
+    st.header('Iniciar sesión')
+    username = st.text_input('Usuario')
+    password = st.text_input('Contraseña', type='password')
+    submit_button = st.form_submit_button(label='Iniciar sesión')
