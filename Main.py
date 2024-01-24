848d63a1860905406809bc79ff0fde5586fd2eef
SergioSKA27 <sergioska81@hotmail.com>
2024-01-24 04:19:40 -0600

    Remove unnecessary 'grow' parameter from Tabs component

--- Main.py
+++ Main.py
@@ -428,7 +428,7 @@ sac.TabsItem(label='Contacto',icon='phone'),
 sac.TabsItem(label='Política de Privacidad',icon='lock'),
 sac.TabsItem(label='Términos y Condiciones',icon='file-earmark-text'),
 sac.TabsItem(label='FAQ',icon='question-circle'),
-],position='bottom',align='center',return_index=True,grow=True)
+],position='bottom',align='center',return_index=True)
 
 
 with open('rsc/html/footer.html') as foo:
