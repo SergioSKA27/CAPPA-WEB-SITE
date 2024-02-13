a6a4430afe88970f3a0d054fc0e2167046ab26ca
SergioSKA27 <sergioska81@hotmail.com>
2024-02-13 06:54:28 -0600

    Añadir comprobación de nulidad para el objeto de usuario

--- pages/app.py
+++ pages/app.py
@@ -79,9 +79,12 @@ if auth() == False and valcookie is not None:
     st.rerun()
 
 
+
+placeholder = st.empty()
+
 if auth():
     #st.session_state['userinfo']
-    if st.session_state.user.is_admin() or st.session_state.user.is_teacher():
+    if st.session_state.user is not None and (st.session_state.user.is_admin() or st.session_state.user.is_teacher()):
         menu_data = [
         {'icon': "bi bi-cpu",'label':"Problemas",'ttip':"Problemas de Programación",
         'submenu':[
@@ -105,7 +108,7 @@ if auth():
         ]}
 
     ]
-    else:
+    elif st.session_state.user is not None:
         menu_data = [
         {'icon': "bi bi-cpu",'label':"Problemas",'ttip':"Problemas de Programación",'id':'Problemas'},
         {'id':'courses','icon': "bi bi-journal-bookmark", 'label':"Cursos",'ttip':"Cursos de Programación y Ciencia de Datos en CAPPA"},
@@ -134,7 +137,7 @@ if auth():
         sticky_mode='sticky', #jumpy or not-jumpy, but sticky or pinned
     )
 
-    if st.session_state.user.is_admin() or st.session_state.user.is_teacher():
+    if st.session_state.user is not None and (st.session_state.user.is_admin() or st.session_state.user.is_teacher()):
         if menu_id == 'subid00':
             st.switch_page('pages/problems_home.py')
 
@@ -177,7 +180,7 @@ if auth():
 
 
 
-    if menu_id == st.session_state.user.usuario:
+    if st.session_state.user is not None and menu_id == st.session_state.user.usuario:
         if 'query' not in st.session_state:
             st.session_state.query = {'Table':'Usuario','id':st.session_state.user.key}
         else:
